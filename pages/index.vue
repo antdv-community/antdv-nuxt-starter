db963d57724f7a28e031811e9367384a7a2aa066
aibayanyu <aibayanyu@qq.com>
2023-07-19 06:36:47 +0800

    fix: add message

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 const checked = shallowRef(true)
+const handleClick = () => {
+  message.info('test')
+}
 </script>
 
 <template>
   <div>
-    <a-button>按钮</a-button>
+    <a-button @click="handleClick">
+      按钮
+    </a-button>
     <a-switch :checked="checked" />
   </div>
 </template>
