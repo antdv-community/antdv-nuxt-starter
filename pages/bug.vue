2c5301f88136ddb045d6e6e0b4ad2206996ba817
undefined <undefined>
2023-12-19 07:01:09 +0800

    chore: change

---
+++ pages/bug.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { CascaderProps } from 'ant-design-vue'
+const options: CascaderProps['options'] = [
+  {
+    value: 'zhejiang',
+    label: 'Zhejiang',
+    children: [
+      {
+        value: 'hangzhou',
+        label: 'Hangzhou',
+        children: [
+          {
+            value: 'xihu',
+            label: 'West Lake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    value: 'jiangsu',
+    label: 'Jiangsu',
+    children: [
+      {
+        value: 'nanjing',
+        label: 'Nanjing',
+        children: [
+          {
+            value: 'zhonghuamen',
+            label: 'Zhong Hua Men',
+          },
+        ],
+      },
+    ],
+  },
+]
+const value = ref<string[]>([])
+</script>
+
+<template>
+  <a-cascader v-model:value="value" :options="options" placeholder="Please select" />
+</template>
